fd9696b3285a7accaf84ac88a9958bb6112511a1
Devin Kelly <devinpkelly@gmail.com>
2018-03-29 16:00:10 -0400

    Final commit for project

--- js/flickr.js
+++ js/flickr.js
@@ -1,26 +1,34 @@
-//Adds pertinent information to pull images from my flickr using API key, and a designated photoset ID//
+$(function() {
+    $.ajax({
+        url: (window.location.protocol === 'https:' ? 'https://secure' : 'http://api') + '.flickr.com/services/rest/',
+        data: {
+            format: 'json',
+            method: 'flickr.photosets.getPhotos',
+            api_key: '3adcf8bb0b80436dfde9f78c5958d441',
+            photoset_id: '72157694293065414'
+        },
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback'
+    }).done(function (result) {
+        var linksContainer = document.getElementById("flickr-gallery");
+        var baseUrl;
 
-$.ajax({
-    url: (window.location.protocol === 'https:' ? 'https://secure' : 'http://api') + '.flickr.com/services/rest/',
-    data: {
-        format: 'json',
-        method: 'flickr.photosets.getPhotos',
-        api_key: '3adcf8bb0b80436dfde9f78c5958d441',
-        photoset_id: '72157691328964952'
-    },
-    dataType: 'jsonp',
-    jsonp: 'jsoncallback'
-}).done(function (result) {
-    var linksContainer = $('#links'),
-    baseUrl;
+    // Add Flickr Photos from FEWD set...
+        $.each(result.photoset.photo, function (index, photo) {
+            baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret;
 
-// Add Flickr Photos from FEWD set...
-    $.each(result.photoset.photo, function (index, photo) {
-        baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret;
-       $('<a/>').append($('<img></img>').prop("src", baseUrl + "_s.jpg"))
-        .prop('href', baseUrl + '_b.jpg')
-        .prop('title', photo.title)
-        .attr('data-gallery', '')
-        .appendTo(linksContainer);
+            // build the div to put everything in
+            var div = document.createElement("div");
+
+            // build the image element
+            var img = document.createElement("img");
+            img.src = baseUrl + "_z.jpg";
+
+            // add the link to the div as a child
+            div.appendChild(img);
+
+            // add the div into the links container
+            linksContainer.appendChild(div);
+        });
     });
 });
